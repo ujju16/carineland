c7922b19976cd2c76cfe0207ace3e2595170d366
ujju16 <jul.darrouza@gmail.com>
2025-11-03 09:34:17 +0100

    fix: format lighthouserc.js with Prettier

--- lighthouserc.js
+++ lighthouserc.js
@@ -15,9 +15,9 @@ module.exports = {
       preset: 'lighthouse:recommended',
       assertions: {
         'categories:performance': ['warn', { minScore: 0.85 }],
-        'categories:accessibility': ['warn', { minScore: 0.90 }],
+        'categories:accessibility': ['warn', { minScore: 0.9 }],
         'categories:best-practices': ['warn', { minScore: 0.85 }],
-        'categories:seo': ['warn', { minScore: 0.90 }],
+        'categories:seo': ['warn', { minScore: 0.9 }],
       },
     },
     upload: {
